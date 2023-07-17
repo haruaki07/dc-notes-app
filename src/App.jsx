f43b46554945a522a154e123bc64743892c3fba0
Haruaki <misbahulk2236@gmail.com>
2023-07-17 04:07:00 +0000

    feat: container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import AppLayout from "./lib/layouts/AppLayout"
+import Container from "./lib/ui/Container"
 
 function App() {
-  return <AppLayout></AppLayout>
+  return (
+    <AppLayout>
+      <Container center>haiyaaa</Container>
+    </AppLayout>
+  )
 }
 
 export default App
---
+++ src/lib/ui/Container/index.jsx
@@ -0,0 +1,20 @@
+import clsx from "clsx"
+import styles from "./container.module.scss"
+
+/** @type {React.FC<React.ComponentProps<HTMLDivElement> & { center?: boolean }>>} */
+const Container = ({ children, center, className, ...restProps }) => {
+  return (
+    <div
+      className={clsx(
+        styles.container,
+        center && styles.containerCenter,
+        className
+      )}
+      {...restProps}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Container
